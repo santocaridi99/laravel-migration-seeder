84ef305995fb0f650df18b699461a45882767f69
santocaridi99 <santocaridi99@gmail.com>
2022-03-07 17:11:29 +0100

    forzato in  model il nome della tabella Travel per poter eseguire il seed relativo

--- app/Travel.php
+++ app/Travel.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Travel extends Model
 {
-    //
-    protected $travel = 'travel';
+    //forzo il nome alla tabella
+    protected $table = 'travels';
 }
