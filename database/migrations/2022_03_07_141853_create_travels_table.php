82760fa34eabefbcd35d546e8e2b5e551d1e4361
santocaridi99 <santocaridi99@gmail.com>
2022-03-07 15:47:27 +0100

    creato migration Table e aiggiunto alla tabella i vari dati  e creato model Travel

---
+++ database/migrations/2022_03_07_141853_create_travels_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTravelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travels', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            // nome pacchetto
+            $table->string("package",100);
+            // destinazione del viaggio
+            $table->string("destination",100);
+            // tipo del pacchetto
+            $table->string("type",100);
+            // prezzo
+            $table->mediumInteger("price");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travels');
+    }
+}
